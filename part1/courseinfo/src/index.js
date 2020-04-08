4836ac529c195a69b58bd0ae7d2273e7923c87ef
Vinura <vinuraeu@gmail.com>
2020-04-08 23:34:23 +0530

    Complete the first step of courseinfo-part1

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  const parts = props.parts;
+  return (
+    <div>
+      {parts.map(p => ( // map returns an array
+        <p key={p.id}>{p.name} {p.exercises}</p>
+      ))}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const total = props.parts.reduce(
+    (total, currentValue) => (
+      total + currentValue.exercises
+    ), props.parts[0].exercises
+  );
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application Development'
+  const parts = [
+    {
+      id: 'part1',
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      id: 'part2',
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      id: 'part3',
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
