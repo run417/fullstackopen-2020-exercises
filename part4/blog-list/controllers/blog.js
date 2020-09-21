bc65ea5488375fb29fb3fdabe495d76ebd4ab379
Vinura <vinuraeu@gmail.com>
2020-09-21 14:18:39 +0530

    Complete exercise 4-19, bloglist expansion step7

--- part4/blog-list/controllers/blog.js
+++ part4/blog-list/controllers/blog.js
@@ -1,7 +1,15 @@
 const blogRouter = require('express').Router();
-
+const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
 const User = require('../models/user');
+
+const getTokenFrom = (request) => {
+    const authorization = request.get('authorization'); // get the authorization header
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7);
+    }
+    return null;
+};
 
 blogRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
@@ -18,8 +26,14 @@ blogRouter.get('/:id', async (request, response) => {
 
 blogRouter.post('/', async (request, response) => {
     const { body } = request;
-    const users = await User.find({});
-    const user = users[1];
+    const token = getTokenFrom(request);
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    if (!token || !decodedToken.id) {
+        // null token or no user id
+        response.status(401).json({ error: 'invalid or missing token' });
+    }
+
+    const user = await User.findById(decodedToken.id);
     const blogData = {
         title: body.title,
         author: body.author,
@@ -31,7 +45,7 @@ blogRouter.post('/', async (request, response) => {
     const savedBlog = await blog.save();
     user.blogs = user.blogs.concat(savedBlog._id);
     await user.save();
-    user.blogs = response.status(201).json(savedBlog);
+    response.json(savedBlog);
 });
 
 blogRouter.put('/:id', async (request, response) => {
--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -20,6 +20,8 @@ const errorHandler = (error, request, response, next) => {
         // eslint-disable-next-line no-else-return
     } else if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message });
+    } else if (error.name === 'JsonWebTokenError') {
+        return response.status(401).json({ error: 'invalid token' });
     }
     next(error);
 };
