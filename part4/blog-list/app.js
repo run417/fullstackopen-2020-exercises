80a9e17eeeef82005d382aca8dd54444ee34cb98
Vinura <vinuraeu@gmail.com>
2020-09-21 13:29:48 +0530

    Implement token based authentication exercise 4-18

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -8,6 +8,7 @@ const userRouter = require('./controllers/user');
 const blogRouter = require('./controllers/blog');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
+const loginRouter = require('./controllers/login');
 
 const app = express();
 
@@ -24,6 +25,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
+app.use('/api/login', loginRouter);
 app.use('/api/users', userRouter);
 app.use('/api/blogs', blogRouter);
 app.use(middleware.unknownEndpoint);
