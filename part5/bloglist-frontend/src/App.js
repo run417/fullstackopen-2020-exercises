b8d3113dbaf16b9097587415c8e1a205188b6596
Vinura <vinuraeu@gmail.com>
2020-09-27 13:55:00 +0530

    Complete exercise 5-2, bloglist frontend, step 2
    - remember user login
    - handle user logout

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -13,10 +13,21 @@ const App = () => {
         blogService.getAll().then((blogs) => setBlogs(blogs));
     }, []);
 
+    useEffect(() => {
+        const loggedBlogUserJson = window.localStorage.getItem(
+            'loggedBlogUser'
+        );
+        if (loggedBlogUserJson) {
+            const user = JSON.parse(loggedBlogUserJson);
+            setUser(user);
+        }
+    }, []);
+
     const handleLogin = async (event) => {
         event.preventDefault();
         try {
             const user = await loginService.login({ username, password });
+            window.localStorage.setItem('loggedBlogUser', JSON.stringify(user));
             setUser(user);
             setUsername('');
             setPassword('');
@@ -24,6 +35,11 @@ const App = () => {
             console.dir(exception.response.data.error);
             alert('Wrong crendentials');
         }
+    };
+
+    const handleLogout = () => {
+        window.localStorage.removeItem('loggedBlogUser');
+        setUser(null);
     };
 
     const loginForm = () => (
@@ -54,7 +70,10 @@ const App = () => {
     const blogList = () => (
         <div>
             <h2>blogs</h2>
-            <p>{user.name} is logged in</p>
+            <p>
+                {user.name} is logged in{' '}
+                <button onClick={handleLogout}>logout</button>
+            </p>
             {blogs.map((blog) => (
                 <Blog key={blog.id} blog={blog} />
             ))}
