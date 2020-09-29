f7cc626b1bd60eee92e809fc93058662b42d7ccf
Vinura <vinuraeu@gmail.com>
2020-09-30 03:46:00 +0530

    Complete exercise 5-7, bloglist frontend, step 7
    - show/hide blog details
    - some css to centre content and set max width

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,46 @@
-import React from 'react';
-const Blog = ({ blog }) => (
-    <div>
-        {blog.title} - {blog.author}
-    </div>
-);
+import React, { useState } from 'react';
+import Toggleable from './Toggleable';
+
+const BlogDetails = ({ blog }) => <div>{blog.url}</div>;
+
+const Blog = ({ blog }) => {
+    const [visible, setVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        setVisible(!visible);
+    };
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5,
+    };
+
+    const hideWhenVisible = { display: visible ? 'none' : '' };
+    const showWhenVisible = { display: visible ? '' : 'none' };
+    return (
+        <div style={blogStyle}>
+            <div style={hideWhenVisible}>
+                <p>
+                    {blog.title} - {blog.author}{' '}
+                    <button onClick={toggleVisibility}>view</button>
+                </p>
+            </div>
+            <div style={showWhenVisible}>
+                <p>
+                    {blog.title} - {blog.author}{' '}
+                    <button onClick={toggleVisibility}>hide</button>
+                </p>
+                <p>{blog.url}</p>
+                <p>
+                    likes {blog.likes} <button>like</button>
+                </p>
+                <p>{blog.author}</p>
+            </div>
+        </div>
+    );
+};
 
 export default Blog;
