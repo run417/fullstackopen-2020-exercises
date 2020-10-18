d104b90fc558e04117a97a327cd5633ebc4c82d3
Vinura <vinuraeu@gmail.com>
2020-10-18 19:33:30 +0530

    Complete exercise 5.13, blog list tests, step 1

---
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Blog from './Blog';
+
+test('renders blog list view', () => {
+    const blog = {
+        title: 'test blog title',
+        author: 'vinura',
+        likes: 4,
+        url: 'example.com',
+        user: {
+            username: 'jsx',
+            name: 'alex',
+        },
+    };
+
+    const component = render(
+        <Blog
+            blog={blog}
+            loggedInUser={blog.user}
+            deleteBlog={jest.fn()}
+            updateBlogLikes={jest.fn()}
+        />
+    );
+    // const blogE
+    const blogListDataRegex = new RegExp(`^${blog.title} - ${blog.author}`);
+    const blogListView = component.container.querySelector('.blogListView');
+    const blogDetails = component.container.querySelector('.blogDetails');
+    expect(blogListView).toHaveTextContent(blogListDataRegex);
+    expect(blogDetails).toHaveStyle('display: none');
+});
