641c0f39a888e92f17a8e46d551f8ca824db5835
Vinura <vinuraeu@gmail.com>
2020-10-20 19:52:58 +0530

    Complete exercise 5.15, blog list tests, step 3

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -62,3 +62,35 @@ test('renders blog likes and url when details are shown', () => {
     expect(blogDetails).toHaveTextContent(blog.url);
     expect(blogDetails).toHaveTextContent(blog.likes);
 });
+
+test('clicking like button twice calls the event handler twice', () => {
+    const blog = {
+        title: 'test blog title',
+        author: 'vinura',
+        likes: 4,
+        url: 'example.com',
+        user: {
+            username: 'jsx',
+            name: 'alex',
+        },
+    };
+
+    const likesMockHandler = jest.fn();
+    const component = render(
+        <Blog
+            blog={blog}
+            loggedInUser={blog.user}
+            deleteBlog={jest.fn()}
+            updateBlogLikes={likesMockHandler}
+        />
+    );
+
+    const viewButton = component.getByText('view');
+    fireEvent.click(viewButton);
+
+    const likeButton = component.getByText('like');
+    fireEvent.click(likeButton);
+    fireEvent.click(likeButton);
+
+    expect(likesMockHandler.mock.calls).toHaveLength(2);
+});
