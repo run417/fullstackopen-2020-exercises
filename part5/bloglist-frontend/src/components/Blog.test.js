f7c4a63ba6cbf99dfc4808ff106919466dab4950
Vinura <vinuraeu@gmail.com>
2020-10-20 14:18:02 +0530

    Complete exercise 5.14, blog list tests, step 2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Blog from './Blog';
 
@@ -31,4 +31,34 @@ test('renders blog list view', () => {
     expect(blogListView).not.toHaveTextContent(blog.url);
     expect(blogListView).not.toHaveTextContent(blog.likes);
     expect(blogDetails).toHaveStyle('display: none');
+});
+
+test('renders blog likes and url when details are shown', () => {
+    const blog = {
+        title: 'test blog title',
+        author: 'vinura',
+        likes: 4,
+        url: 'example.com',
+        user: {
+            username: 'jsx',
+            name: 'alex',
+        },
+    };
+
+    const component = render(
+        <Blog
+            blog={blog}
+            loggedInUser={blog.user}
+            deleteBlog={jest.fn()}
+            updateBlogLikes={jest.fn()}
+        />
+    );
+
+    const blogDetails = component.container.querySelector('.blogDetails');
+    const viewButton = component.getByText('view');
+    fireEvent.click(viewButton);
+
+    expect(blogDetails).not.toHaveStyle('display: none');
+    expect(blogDetails).toHaveTextContent(blog.url);
+    expect(blogDetails).toHaveTextContent(blog.likes);
 });
