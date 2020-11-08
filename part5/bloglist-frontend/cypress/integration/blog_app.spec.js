60eeed828bd1f1fc8b2bf07d1cbee5f6b8f7f299
Vinura <vinuraeu@gmail.com>
2020-11-08 13:34:21 +0530

    Complete exercise 5.20, bloglist end to end testing, step 4

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -8,11 +8,37 @@ Cypress.Commands.add('login', ({ username, password }) => {
     });
 });
 
+Cypress.Commands.add(
+    'createBlogAsUser',
+    ({ title, author, url, username, password }) => {
+        let token = null;
+        cy.request('POST', 'http://localhost:3003/api/login', {
+            username,
+            password,
+        }).then((response) => {
+            token = response.body.token;
+            cy.request({
+                url: 'http://localhost:3003/api/blogs',
+                method: 'POST',
+                body: { title, author, url },
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+        });
+    }
+);
+
 describe('Blog app', function () {
     beforeEach(function () {
         cy.request('POST', 'http://localhost:3003/api/testing/reset');
+
         const user = { username: 'vinura', name: 'Vinura', password: 'secret' };
         cy.request('POST', 'http://localhost:3003/api/users', user);
+
+        const user2 = { username: 'exos', name: 'Alexa', password: 'secret2' };
+        cy.request('POST', 'http://localhost:3003/api/users', user2);
+
         cy.visit('http://localhost:3000');
     });
 
@@ -53,6 +79,29 @@ describe('Blog app', function () {
                 cy.contains('a new blog cypress blog title added');
                 cy.get('.blogList').contains('cypress blog title');
             });
+        });
+    });
+
+    describe('Existing blogs', function () {
+        beforeEach(function () {
+            cy.createBlogAsUser({
+                title: 'new blog as different user',
+                author: 'cypress',
+                url: 'example.com',
+                username: 'exos',
+                password: 'secret2',
+            });
+            // login as a different user
+            cy.login({ username: 'vinura', password: 'secret' });
+        });
+
+        it.only('user can like blog', function () {
+            cy.contains('new blog as different user')
+                .contains('button', 'view')
+                .click();
+            cy.contains('likes').should('contain', '0');
+            cy.contains('button', 'like').click();
+            cy.contains('likes').should('contain', '1');
         });
     });
 });
